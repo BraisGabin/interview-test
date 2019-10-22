95575a53c3e7ad370760d54e4e7047252fc89cbf
Brais Gabín <braisgabin@gmail.com>
2019-10-22 10:54:51 +0200

    Create RxViewModel

---
+++ app/src/main/java/com/braisgabin/interview/kapten/utils/RxViewModel.kt
@@ -0,0 +1,12 @@
+package com.braisgabin.interview.kapten.utils
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+open class RxViewModel: ViewModel() {
+  protected val disposable = CompositeDisposable()
+
+  override fun onCleared() {
+    disposable.dispose()
+  }
+}
