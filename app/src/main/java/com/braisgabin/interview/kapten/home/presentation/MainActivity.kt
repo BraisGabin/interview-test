fb8c7e6a2667d8983dc15140a456f05071341c30
Brais Gabín <braisgabin@gmail.com>
2019-10-31 14:51:31 +0100

    Add states for loading and error

--- app/src/main/java/com/braisgabin/interview/kapten/home/presentation/MainActivity.kt
+++ app/src/main/java/com/braisgabin/interview/kapten/home/presentation/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.braisgabin.interview.kapten.home.presentation
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.braisgabin.interview.kapten.R
-import com.braisgabin.interview.kapten.entity.Trip
 import com.braisgabin.interview.kapten.appComponent
+import com.braisgabin.interview.kapten.entity.Trip
 import com.braisgabin.interview.kapten.home.presentation.feature.State
 import dagger.Subcomponent
 import io.reactivex.disposables.CompositeDisposable
@@ -35,6 +36,8 @@ class MainActivity : AppCompatActivity(), TripAdapter.Listener {
     recyclerView.addItemDecoration(DividerItemDecoration(this, RecyclerView.VERTICAL).apply {
       setDrawable(AppCompatResources.getDrawable(this@MainActivity, R.drawable.divider)!!)
     })
+
+    retry.setOnClickListener { presenter.events.accept(HomeIntents.Retry) }
   }
 
   override fun onStart() {
@@ -52,9 +55,10 @@ class MainActivity : AppCompatActivity(), TripAdapter.Listener {
 
   private fun render(state: State) {
     when (state) {
-      State.Load -> {}
-      State.Error -> {}
+      State.Load -> allGoneExcept(progressBar)
+      State.Error -> allGoneExcept(retry)
       is State.Data -> {
+        allGoneExcept(recyclerView)
         val adapter = (recyclerView.adapter as? TripAdapter ?: TripAdapter(this))
         adapter.submitList(state.trips)
         if (recyclerView.adapter !== adapter) {
@@ -66,6 +70,16 @@ class MainActivity : AppCompatActivity(), TripAdapter.Listener {
 
   override fun clickListener(trip: Trip) {
     TODO("not implemented")
+  }
+
+  private fun allGoneExcept(view: View) {
+    progressBar.goneIfNot(view)
+    retry.goneIfNot(view)
+    recyclerView.goneIfNot(view)
+  }
+
+  private fun View.goneIfNot(view: View) {
+    this.visibility = if (this == view) View.VISIBLE else View.GONE
   }
 
   @Subcomponent
